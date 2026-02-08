25873d25003e49cf5fe135dd845f22362841d95a
Aleksandr Ivanov <sasha19337@mail.ru>
2026-02-08 22:19:21 +0300

    add video

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,8 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.open;
@@ -19,6 +21,12 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.remote  = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         open(baseUrl);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
