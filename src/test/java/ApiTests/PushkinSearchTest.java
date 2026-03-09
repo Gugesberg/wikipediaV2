24b2158633a397d45cef7db0ef3bc0489a045626
Aleksandr Ivanov <sasha19337@mail.ru>
2026-03-09 21:55:14 +0300

    удалил лишнее, добавил новый тест

--- src/test/java/ApiTests/PushkinSearchTest.java
+++ src/test/java/ApiTests/PushkinSearchTest.java
@@ -7,18 +7,33 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
 public class PushkinSearchTest {
+    static String BaseURL = "https://ru.wikipedia.org/api/rest_v1";
 
     @Test
-    void chechDescription(){
+    void checkDescription(){
         given()
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
                 .param("redirect", "false")
                 .when()
-                .get("https://ru.wikipedia.org/api/rest_v1/page/summary/Пушкин?redirect=false")
+                .get( BaseURL +"/page/summary/Пушкин?redirect=false")
                 .then()
                 .log().all()
                 .statusCode(200)
                 .body("description",equalTo("русский поэт, драматург и прозаик (1799–1837)"));
+
+    }
+
+    @Test
+    void checkIncorrectSearch(){
+        given()
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                .param("redirect", "false")
+                .when()
+                .get(BaseURL +"/page/summary/ывпаывпвав?redirect=false")
+                .then()
+                .log().all()
+                .statusCode(404);
+
 
     }
 }
