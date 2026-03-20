8140438d1dadcc36628e379f89df261f78c5b261
Aleksandr Ivanov <sasha19337@mail.ru>
2026-03-20 15:35:39 +0300

    Добавил listener для красивого отображения api тестов в allure

--- src/test/java/ApiTests/restfulBooker/CreateToken.java
+++ src/test/java/ApiTests/restfulBooker/CreateToken.java
@@ -1,12 +1,15 @@
 package apiTests.restfulBooker;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import models.PostTokenRequestModel;
 import models.PostTokenResponseModel;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.post;
 import static io.restassured.http.ContentType.JSON;
+import static io.restassured.http.ContentType.fromContentType;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,9 +23,13 @@ public class CreateToken {
         postTokenRequestModel.setUsername("admin");
         postTokenRequestModel.setPassword("password123");
         PostTokenResponseModel response = given()
+                .filter(withCustomTemplates())
                 .body(postTokenRequestModel)
                 .contentType(JSON)
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                .log().uri()
+                .log().body()
+                .log().headers()
                 .when()
                 .post(BaseURL)
                 .then()
@@ -39,9 +46,13 @@ public class CreateToken {
         postTokenRequestModel.setUsername("sasha");
         postTokenRequestModel.setPassword("password");
         PostTokenResponseModel response = given()
+                .filter(withCustomTemplates())
                 .body(postTokenRequestModel)
                 .contentType(JSON)
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                .log().uri()
+                .log().body()
+                .log().headers()
                 .when()
                 .post(BaseURL)
                 .then()
